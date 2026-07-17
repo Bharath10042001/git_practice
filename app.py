036f3bd7bd647b05fdf0516db685227eb907f7a4
Bharath Bandla <bharathbandla490@gmail.com>
2026-07-17 16:40:48 +0530

    updated app.py from info branch

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 x = "I am Git"
 y = 'I am GitHub'
 
-def log(z):
-    print(z + '\n'+ 'I was login was Successful')
+def log(i):
+    print(i + '\n'+ 'I was login was Successful')
 
 def emps_details(a, b):
     print(f'{a + "\n" + b}')
