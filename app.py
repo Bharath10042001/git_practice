48a4230a6bbbd1bde154b7cc19093bc3c17005fc
Bharath Bandla <bharathbandla490@gmail.com>
2026-07-17 16:49:37 +0530

    changed the print statement of log

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ x = "I am Git"
 y = 'I am GitHub'
 
 def log(i):
-    print(i + '\n'+ 'I was login was Successful')
+    print(i + '\n'+ 'and my loggin was Successful')
 
 def emps_details(a, b):
     print(f'{a + "\n" + b}')
