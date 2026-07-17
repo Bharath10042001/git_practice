04b88ba1205b02db077e4358c0220dd4ed799782
Bharath Bandla <bharathbandla490@gmail.com>
2026-07-17 16:38:17 +0530

    updated the both app and test files

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 x = "I am Git"
 y = 'I am GitHub'
 
-def log():
-    print('I was login was Successful')
+def log(a):
+    print(a + '\n'+ 'I was login was Successful')
 
 def emps_details(a, b):
     print(f'{a + "\n" + b}')
--- test.py
+++ test.py
@@ -1,9 +1,14 @@
-from app import x, y, emps_details
+from app import x, y, emps_details, log
 
 a = "I am Ravi Model!"
 b = 'I am Sai'
 
 if __name__ == '__main__':
+    print('------employees---------')
     emps_details(x, a)
     print()
     emps_details(y, b)
+    print('------log---------')
+    log(a)
+    print()
+    log(b)
