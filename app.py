7bd972b6edad93d6fef43d26246a135c1e9e9977
Bharath Bandla <bharathbandla490@gmail.com>
2026-07-17 16:15:21 +0530

    initial created

---
+++ app.py
@@ -0,0 +1,7 @@
+x = "I am Git"
+
+y = 'I am GitHub'
+
+if __name__ == '__main__':
+    print(x)
+    print(y)
---
+++ test.py
@@ -0,0 +1,8 @@
+from app import x, y
+
+a = "I am Ravi Model"
+b = 'I am Gopi The Fire!'
+
+print(f'{x + "\n" + a}')
+print()
+print(f'{y + "\n" + b}')
